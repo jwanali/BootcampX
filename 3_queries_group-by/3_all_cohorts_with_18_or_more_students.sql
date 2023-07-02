9e01737247486026a39737d87e8348a82e58fa19
jwanali <ciwan_ali@hotmail.com>
2023-07-02 18:58:05 -0500

    implement assignments Queries 3 - Group By

---
+++ 3_queries_group-by/3_all_cohorts_with_18_or_more_students.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name as cohort_name, count(students.*)as student_count
+FROM cohorts 
+JOIN students 
+ON students.cohort_id = cohorts.id
+GROUP BY cohorts.name
+HAVING count(students.*) >= 18
+ORDER BY student_count;
