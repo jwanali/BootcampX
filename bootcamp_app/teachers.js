782441f8e3900162264bf0599a472238f9e231ac
jwanali <ciwan_ali@hotmail.com>
2023-07-19 17:59:23 -0500

    Parameterized Query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,14 +6,16 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+const cohortName =`${process.argv[2] || 'JUL02'}`;
+values = [cohortName]
 pool.query(`SELECT teachers.name as teacher, cohorts.name
 FROM teachers 
 JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
 JOIN students ON assistance_requests.student_id = students.id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 group by teachers.name,  cohorts.name
-ORDER BY teachers.name`)
+ORDER BY teachers.name`,values )
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name}: ${user.teacher} `)
